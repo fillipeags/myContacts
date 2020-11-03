ae014036af9e96f9c842e534ce5fdb91c26bf99f
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2020-11-03 10:34:36 -0300

    show and delete routes

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,18 +1,39 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Fillipe',
     email: 'fillipe@mail.com',
     phone: '1212121232',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+
+  {
+    id: v4(),
+    name: 'Jose',
+    email: 'jose@mail.com',
+    phone: '12121212121232',
+    category_id: v4(),
   },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
   }
 }
 
