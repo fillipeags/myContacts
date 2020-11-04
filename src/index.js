be5107a8e0f381dce658dce05e24536e3ad658db
Fillipe Augusto <38387593+fillipeags@users.noreply.github.com>
2020-11-04 10:26:13 -0300

    create contact route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = require('./routes');
+const routes = require('./routes');
 
 const app = express();
-app.use(router);
+
+app.use(express.json());
+app.use(routes);
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('🔥Server started running at http://localhost:3000'));
