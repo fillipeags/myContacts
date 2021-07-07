2595bed09b186b422e97ab710db6fbe23189fe91
Fillipe <fillipe484@gmail.com>
2021-07-07 17:05:36 -0300

    ContactList header

--- frontend/src/assets/styles/themes/default.js
+++ frontend/src/assets/styles/themes/default.js
@@ -1,3 +1,12 @@
 export default {
-  backgroundColor: '#f6f5fc',
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#ebe3ff',
+      light: '#6674f4',
+      main: '#5061FC',
+      dark: '#3346f0',
+    },
+
+  },
 };
