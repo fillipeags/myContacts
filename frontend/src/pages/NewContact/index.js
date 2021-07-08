cb97f15135f0b3a8c35c84ab9ca182d77ae7e506
Fillipe <fillipe484@gmail.com>
2021-07-08 10:20:36 -0300

    feat: Button Component

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -13,6 +14,9 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+
+      <Button type="button">Salvar alteracoes</Button>
+      <Button type="button" disabled>Salvar alteracoes</Button>
 
     </>
   );
