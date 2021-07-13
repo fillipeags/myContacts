86a5eb881bd9dd8f5afa6a6c9ebcaad46fb8f210
Fillipe <fillipe484@gmail.com>
2021-07-12 21:03:56 -0300

    feat: Loader Component

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -6,17 +6,18 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+// import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
+      <Loader />
+      {/* <Modal danger /> */}
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
-
-      <Modal danger />
 
       <Header>
         <strong>3 contatos</strong>
