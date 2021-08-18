fbac2892417ca7e647b315a7c4f886e9e9b45ffb
Fillipe <fillipe484@gmail.com>
2021-08-18 20:08:48 -0300

    feat: Set CORS and and errorHandler middlewares

--- backend/src/app/controllers/ContactController.js
+++ backend/src/app/controllers/ContactController.js
@@ -1,86 +1,86 @@
 const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
-  async index(request, response) {
-    // Listar todos os registros
-    const { orderBy } = request.query;
-    const contacts = await ContactsRepository.findAll(orderBy);
+	async index(request, response) {
+		// Listar todos os registros
+		const { orderBy } = request.query;
+		const contacts = await ContactsRepository.findAll(orderBy);
 
-    response.json(contacts);
-  }
+		response.json(contacts);
+	}
 
-  async show(request, response) {
-    // Obter UM registro
-    const { id } = request.params;
+	async show(request, response) {
+		// Obter UM registro
+		const { id } = request.params;
 
-    const contact = await ContactsRepository.findById(id);
+		const contact = await ContactsRepository.findById(id);
 
-    if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
-    }
+		if (!contact) {
+			return response.status(404).json({ error: 'User not found' });
+		}
 
-    response.json(contact);
-  }
+		response.json(contact);
+	}
 
-  async store(request, response) {
-    // Criar um registro
-    const {
-      name, email, phone, category_id,
-    } = request.body;
+	async store(request, response) {
+		// Criar um registro
+		const {
+			name, email, phone, category_id,
+		} = request.body;
 
-    if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
-    }
+		if (!name) {
+			return response.status(400).json({ error: 'Name is required' });
+		}
 
-    const contactExists = await ContactsRepository.findByEmail(email);
+		const contactExists = await ContactsRepository.findByEmail(email);
 
-    if (contactExists) {
-      return response.status(400).json({ error: 'This email has already been token' });
-    }
+		if (contactExists) {
+			return response.status(400).json({ error: 'This email has already been token' });
+		}
 
-    const contact = await ContactsRepository.create({
-      name, email, phone, category_id,
-    });
+		const contact = await ContactsRepository.create({
+			name, email, phone, category_id,
+		});
 
-    response.json(contact);
-  }
+		response.json(contact);
+	}
 
-  async update(request, response) {
-    // Editar um registro
-    const { id } = request.params;
-    const {
-      name, email, phone, category_id,
-    } = request.body;
+	async update(request, response) {
+		// Editar um registro
+		const { id } = request.params;
+		const {
+			name, email, phone, category_id,
+		} = request.body;
 
-    const contactExists = await ContactsRepository.findById(id);
-    if (!contactExists) {
-      return response.status(404).json({ error: 'User not found' });
-    }
+		const contactExists = await ContactsRepository.findById(id);
+		if (!contactExists) {
+			return response.status(404).json({ error: 'User not found' });
+		}
 
-    if (!name) {
-      return response.status(400).json({ error: 'Name is required' });
-    }
+		if (!name) {
+			return response.status(400).json({ error: 'Name is required' });
+		}
 
-    const contactByEmail = await ContactsRepository.findByEmail(email);
+		const contactByEmail = await ContactsRepository.findByEmail(email);
 
-    if (contactByEmail && contactByEmail.id !== id) {
-      return response.status(400).json({ error: 'This email has already been token' });
-    }
+		if (contactByEmail && contactByEmail.id !== id) {
+			return response.status(400).json({ error: 'This email has already been token' });
+		}
 
-    const contact = await ContactsRepository.update(id, {
-      name, email, phone, category_id,
-    });
+		const contact = await ContactsRepository.update(id, {
+			name, email, phone, category_id,
+		});
 
-    response.json(contact);
-  }
+		response.json(contact);
+	}
 
-  async delete(request, response) {
-    // Deletar um registro
-    const { id } = request.params;
+	async delete(request, response) {
+		// Deletar um registro
+		const { id } = request.params;
 
-    await ContactsRepository.delete(id);
-    response.sendStatus(204);
-  }
+		await ContactsRepository.delete(id);
+		response.sendStatus(204);
+	}
 }
 
 // Singleton
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
+app.use(errorHandler);
 
-// Error Handler (Middleware)
-app.use((error, request, response, next) => {
-	console.log(error);
-	response.sendStatus(500);
-});
-
-// eslint-disable-next-line no-console
 app.listen(3001, () => console.log('🔥 Server started running at http://localhost:3001'));
