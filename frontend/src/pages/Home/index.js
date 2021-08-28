8941e3888fd6964a57890c360f53570d7bde0719
Fillipe <fillipe484@gmail.com>
2021-08-28 20:13:22 -0300

    feat: implement Contacts Service

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -10,7 +10,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContactsService from '../../services/ContactsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -26,13 +26,9 @@ export default function Home() {
     async function loadContacts() {
       try {
         setIsLoading(true);
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
-
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
+        setContacts(contactsList);
       } catch (error) {
         console.log('error', error);
       } finally {
