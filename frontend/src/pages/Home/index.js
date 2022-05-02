fe9832a99cfa4bdd3c3f618876aa378fa83713c9
Fillipe Augusto Siqueira Gomes <fillipeags@Fillipes-MacBook-Air.local>
2022-05-02 17:20:29 -0300

    fix: solving infinite loop on useEffect to loadContacts

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-import { useEffect, useState, useMemo } from 'react';
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
 import { Link } from 'react-router-dom';
 import {
   Container,
@@ -31,7 +33,7 @@ export default function Home() {
     contact.name.toLowerCase().includes(searchTerm.toLowerCase())
   )), [contacts, searchTerm]);
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
 
@@ -44,10 +46,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [orderBy]);
+
   useEffect(() => {
     loadContacts();
-  }, [orderBy]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrderBy(
