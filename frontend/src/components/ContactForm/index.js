d4381936fb2426a745cdb396c86ee359a6f355e6
Fillipe Augusto Siqueira Gomes <fillipeags@Fillipes-MacBook-Air.local>
2022-05-04 21:04:42 -0300

    feat: dealing with categories fetch loading error

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -29,8 +29,12 @@ export default function ContactForm({ buttonLabel }) {
 
   useEffect(() => {
     async function loadCategories() {
-      const categoriesList = await CategoriesService.listCategories();
-      setCategories(categoriesList);
+      try {
+        const categoriesList = await CategoriesService.listCategories();
+        setCategories(categoriesList);
+      } catch {
+
+      }
     }
 
     loadCategories();
