7636931108d5982820b8b9903ff90af9e53e524b
Fillipe Augusto <fillipeags@Fillipes-MacBook-Air.local>
2022-05-04 22:40:03 -0300

    feat: loading status in formGroup

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -20,6 +20,7 @@ export default function ContactForm({ buttonLabel }) {
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
   const [categories, setCategories] = useState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
 
   const {
     setError, removeError, getErrorMessageByFieldName, errors,
@@ -32,8 +33,8 @@ export default function ContactForm({ buttonLabel }) {
       try {
         const categoriesList = await CategoriesService.listCategories();
         setCategories(categoriesList);
-      } catch {
-
+      } catch { } finally {
+        setIsLoadingCategories(false);
       }
     }
 
@@ -101,10 +102,11 @@ export default function ContactForm({ buttonLabel }) {
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup isLoading={isLoadingCategories}>
         <Select
           value={categoryId}
           onChange={(event) => setCategoryId(event.target.value)}
+          disabled={isLoadingCategories}
         >
           <option value="">Sem Categoria</option>
 
